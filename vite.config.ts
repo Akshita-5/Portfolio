1a06e7bb4c97c3468735eab3086950a9ef9c6970
Akshita-5 <akshita.mitmpl2022@learner.manipal.edu>
2025-07-08 01:48:45 +0530

    fixed order for hosting again

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist',
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
   },
-})
+  root: ".", // <-- root is now current directory
+  build: {
+    outDir: "dist", // output directory
+    emptyOutDir: true,
+  },
+});
